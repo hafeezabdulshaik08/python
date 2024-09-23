4973d83e6e35d70349d4d2d831b880f135906e56
SHAIK ABDUL HAFEEZ <168513325+hafeezabdulshaik08@users.noreply.github.com>
2024-09-23 16:56:22 +0530

    Update 29-07 python.py

--- 29-07 python.py
+++ 29-07 python.py
@@ -69,5 +69,40 @@ if j==2:
 else:
 	print(" not a prime number")	
 				
-
+#23-09-2027
+list=[2,6,8,10]
+#print("sum:",sum(list))
+#print("avg:",sum(list)/len(list))
+#2print(list[::-1])
+#list.reverse()
+#print(list)
+#3def swap(n):
+	#n[0],n[-1]=n[-1],n[0]
+	#return n
+#@list=[22,44,66,99]
+#print(swap(list))
+#def fococ(l,char):
+#	for i,c in enumerate(l):	
+#		if c==char:
+#			return i
+#	return "v"	
+#list=[2,6,8,10]
+#print(list)
+#c=int(input("enter the elementto be removed:"))
+#a=fococ(list,c)
+#if a!="v":
+#	list.pop(a)
+#else:
+#	print("the entered element is not in the list:")
+#print(list)	 		
+list=[2,6,8,10]
+even=[]
+odd=[]
+for i in list:
+	if i%2==0:
+		even.append(i)
+	else:
+		odd.append(i)	
+print(even)
+print(odd)		
 
