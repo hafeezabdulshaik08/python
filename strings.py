583a4d43d24fed89e5625bd2297216bc505f436a
SHAIK ABDUL HAFEEZ <168513325+hafeezabdulshaik08@users.noreply.github.com>
2024-09-30 15:41:32 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,21 @@
+str1="hello"
+str2="world"
+str3=str1+str2
+print(str3)
+print(str3*5)
+print('e' in str1)
+print("w" not in str2)
+print(str3[1:4])
+print(str3[:6])
+print(str3[1::2])
+print(str3[::-1])
+print(str3.capitalize())
+print(str3.upper())
+print(str3.isupper())
+print(str3.islower())
+print(str1.title())
+txt="my name is{name},i am {age}".format(name="vamsi",age=19)
+sting1="f11111"
+print(sting1.isalnum())
+print(str1.isalpha())
+print(str1.isdigit())
